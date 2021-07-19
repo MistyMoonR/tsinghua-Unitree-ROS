f27d3337c2c68409a0ff3d6a2bb71f06ef87ae5a
MistyMoon <33083971+MistyMoonR@users.noreply.github.com>
2021-07-19 18:35:05 +0800

    Update ROS-melodic.sh

--- scripts/ROS-melodic.sh
+++ scripts/ROS-melodic.sh
@@ -4,27 +4,36 @@
 #  换清华源 用于提高速度 (非大陆用户自行删除) .... 
 #---------------------------------------------------#
 
+echo
+/bin/echo -e "\e[1;36m !-----------------------------------------------------------------------!\e[0m"
+/bin/echo -e "\e[1;36m ! 将会执行换清华源 + 软件更新 + ROS melodic 和依懒的安装                      !\e[0m"
+/bin/echo -e "\e[1;36m ! 此过程会花费较长时间，取决于你电脑的网速和性能，不如泡杯Java喝                  !\e[0m"
+/bin/echo -e "\e[1;36m ! 3秒钟后将会执行                                                         !\e[0m"
+/bin/echo -e "\e[1;36m !-----------------------------------------------------------------------!\e[0m"
+echo
 
-sudo echo "[*] 将原sources.list 备份为 sources.list.bak"
+sleep 3s
+
 sudo cp /etc/apt/sources.list /etc/apt/sources.list.bak
-
-sudo echo "[*] 正在将 清华源 覆盖到/etc/apt/sources.list..."
 
 sudo rm -rf /etc/apt/sources.list
 
-sudo touch /etc/apt/sources.list
+# 由于linux的feature，sudo 只对 echo起作用，没有对>>起作用。因此采用touch然后mv方法
+touch ~/Desktop/sources.list
 
-sudo echo "# 清华镜像源" >> /etc/apt/sources.list
+sudo echo "# 清华镜像源" >> ~/Desktop/sources.list
 
-sudo echo "# 默认注释了源码镜像以提高 apt update 速度，如有需要可自行取消注释" >> /etc/apt/sources.list
-sudo echo "deb https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ bionic main restricted universe multiverse" >> /etc/apt/sources.list
-sudo echo "# deb-src https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ bionic main restricted universe multiverse" >> /etc/apt/sources.list
-sudo echo "deb https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ bionic-updates main restricted universe multiverse" >> /etc/apt/sources.list
-sudo echo "# deb-src https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ bionic-updates main restricted universe multiverse" >> /etc/apt/sources.list
-sudo echo "deb https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ bionic-backports main restricted universe multiverse" >> /etc/apt/sources.list
-sudo echo "# deb-src https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ bionic-backports main restricted universe multiverse" >> /etc/apt/sources.list
-sudo echo "deb https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ bionic-security main restricted universe multiverse" >> /etc/apt/sources.list
-sudo echo "# deb-src https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ bionic-security main restricted universe multiverse" >> /etc/apt/sources.list
+sudo echo "# 默认注释了源码镜像以提高 apt update 速度，如有需要可自行取消注释" >> ~/Desktop/sources.list
+sudo echo "deb https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ bionic main restricted universe multiverse" >> ~/Desktop/sources.list
+sudo echo "# deb-src https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ bionic main restricted universe multiverse" >> ~/Desktop/sources.list
+sudo echo "deb https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ bionic-updates main restricted universe multiverse" >> ~/Desktop/sources.list
+sudo echo "# deb-src https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ bionic-updates main restricted universe multiverse" >> ~/Desktop/sources.list
+sudo echo "deb https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ bionic-backports main restricted universe multiverse" >> ~/Desktop/sources.list
+sudo echo "# deb-src https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ bionic-backports main restricted universe multiverse" >> ~/Desktop/sources.list
+sudo echo "deb https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ bionic-security main restricted universe multiverse" >> ~/Desktop/sources.list
+sudo echo "# deb-src https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ bionic-security main restricted universe multiverse" >> ~/Desktop/sources.list
+
+sudo mv ~/Desktop/sources.list /etc/apt/
 
 echo
 /bin/echo -e "\e[1;36m !-----------------------------------------------------------------------!\e[0m"
@@ -77,17 +86,17 @@ echo
 echo
 
 # 下面依懒可选，自行删除
-# sudo apt-get install -y python-rosinstall python-rosinstall-generator python-wstool build-essential
+sudo apt-get install -y python-rosinstall python-rosinstall-generator python-wstool build-essential
 
-# sudo apt-get install -y ros-melodic-navigation ros-melodic-robot-localization ros-melodic-robot-state-publisher
+sudo apt-get install -y ros-melodic-navigation ros-melodic-robot-localization ros-melodic-robot-state-publisher
 
-# sudo apt-get install -y ros-melodic-ddynamic-reconfigure       
+sudo apt-get install -y ros-melodic-ddynamic-reconfigure       
 
-# sudo apt-get install -y libpcap-dev
+sudo apt-get install -y libpcap-dev
 
 echo
 /bin/echo -e "\e[1;36m !-----------------------------------------------------------------------!\e[0m"
-/bin/echo -e "\e[1;36m ! Installation Completed                                                !\e[0m"
+/bin/echo -e "\e[1;36m ! All Installation Completed                                                !\e[0m"
 /bin/echo -e "\e[1;36m !-----------------------------------------------------------------------!\e[0m"
 echo
 
